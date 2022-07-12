43f78af6b3eba8d5fc076dfa7a24632ec14d7b8f
Syed Mazhar Ali <87525103+Amfaa@users.noreply.github.com>
2022-07-12 15:04:41 +0530

    adding food tag

--- src/products.js
+++ src/products.js
@@ -4,7 +4,7 @@ import style from "./product.css"
 const Products = ({title, calories, image, ingredients}) =>{
     return (
         <div className= {style.box}>
-       
+        <h1> Food </h1>
         <h2 className= {style.title}>{ title}</h2>
       <ul> {ingredients.map((ingnt)=> (
           <li className= {style.text}> {ingnt.text}</li>
